dc69e9572f5e9a47effb06cebe5036017079988f
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2023-04-20 14:24:59 +0300

    add mock data and courses list

--- src/app/courses-page/course/course.component.ts
+++ src/app/courses-page/course/course.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Course } from './course';
 
 @Component({
     selector: 'app-course',
     templateUrl: './course.component.html',
     styleUrls: ['./course.component.scss'],
 })
-export class CourseComponent {}
+export class CourseComponent {
+    @Input() course?: Course;
+}
