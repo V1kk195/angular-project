3b9deb958f3fd65094634e2b3f6a38bea0487555
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2023-04-23 19:24:22 +0300

    add tests for CourseList

--- src/app/courses-page/courses-list/courses-list.component.spec.ts
+++ src/app/courses-page/courses-list/courses-list.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesListComponent } from './courses-list.component';
 import { CourseComponent } from '../course/course.component';
+import { courses } from './mock-courses';
 
 describe('CoursesListComponent', () => {
     let component: CoursesListComponent;
@@ -19,5 +20,36 @@ describe('CoursesListComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should show list of courses', () => {
+        const componentEl: HTMLElement = fixture.nativeElement;
+        const listElements = componentEl.querySelectorAll('li');
+
+        expect(listElements).toHaveSize(courses.length);
+    });
+
+    it('should have working button "Load more"', () => {
+        const componentEl: HTMLElement = fixture.nativeElement;
+        const buttonEl: HTMLButtonElement | null =
+            componentEl.querySelector('.load-button');
+        const consoleSpy = spyOn(console, 'log');
+
+        expect(buttonEl?.textContent).toEqual('Load more');
+
+        buttonEl?.click();
+
+        expect(consoleSpy).toHaveBeenCalled();
+    });
+
+    it('should delete course on click Delete button', function () {
+        const componentEl: HTMLElement = fixture.nativeElement;
+        const buttonEl: HTMLButtonElement | null =
+            componentEl.querySelector('.delete-button');
+        const deleteFunction = spyOn(component, 'onDeleteCourse');
+
+        buttonEl?.click();
+
+        expect(deleteFunction).toHaveBeenCalled();
     });
 });
