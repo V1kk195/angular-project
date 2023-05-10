8cc4f1d52bcd91d1fafefefcf07aa5132c4be58a
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2023-05-10 17:07:43 +0300

    add not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoursesPageComponent } from './courses-page/courses-page.component';
 import { AddCourseComponent } from './courses-page/add-course/add-course.component';
 import { EditCourseComponent } from './courses-page/edit-course/edit-course.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { ROUTES_NAMES } from './core/routes';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/courses', pathMatch: 'full' },
-    { path: 'courses', component: CoursesPageComponent, title: 'Courses' },
-    { path: 'courses/:id', component: EditCourseComponent },
+    { path: '', redirectTo: `/${ROUTES_NAMES.courses}`, pathMatch: 'full' },
     {
-        path: 'courses/new',
+        path: ROUTES_NAMES.courses,
+        component: CoursesPageComponent,
+        title: 'Courses',
+    },
+    {
+        path: ROUTES_NAMES.addCourse,
         component: AddCourseComponent,
         title: 'Add new course',
     },
+    { path: `${ROUTES_NAMES.courses}/:id`, component: EditCourseComponent },
+    { path: '**', component: NotFoundPageComponent },
 ];
 
 @NgModule({
---
+++ src/app/not-found-page/not-found-page.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { NotFoundPageComponent } from './not-found-page.component';
+
+@NgModule({
+    declarations: [NotFoundPageComponent],
+    imports: [SharedModule],
+})
+export class NotFoundPageModule {}
