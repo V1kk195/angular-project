910494bfadaf8867218ebdf3b349c0abdf476d64
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2023-05-11 12:47:01 +0300

    refactor files

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { AuthService } from '../core/auth.service';
+import { AuthService } from '../core/auth/auth.service';
 import { Router } from '@angular/router';
 import { ROUTES_NAMES } from '../core/routes';
 
