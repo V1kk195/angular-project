fe7e784e6dacbecd9538d05cf94273a15c63830e
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2023-05-12 12:37:06 +0300

    refactor courses

--- src/app/courses-page/courses-list/courses-list.component.ts
+++ src/app/courses-page/courses-list/courses-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Course } from '../../types/course';
 import { CoursesService } from '../../core/courses-services/courses.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-courses-list',
     templateUrl: './courses-list.component.html',
     styleUrls: ['./courses-list.component.scss'],
 })
-export class CoursesListComponent implements OnInit {
+export class CoursesListComponent implements OnInit, OnDestroy {
     public courses: Course[] = [];
     private nextStartPoint = 0;
+    private subs: Subscription[] = [];
 
     constructor(private coursesService: CoursesService) {}
 
@@ -17,11 +19,17 @@ export class CoursesListComponent implements OnInit {
         this.getCourses();
     }
 
+    public ngOnDestroy() {
+        this.subs.forEach((item) => item.unsubscribe());
+    }
+
     private getCourses(start?: number): void {
-        this.coursesService.getCourses(start).subscribe((data) => {
+        const sub = this.coursesService.getCourses(start).subscribe((data) => {
             this.courses = [...this.courses, ...data];
             this.nextStartPoint = this.nextStartPoint + 5;
         });
+
+        this.subs.push(sub);
     }
 
     public onLoadMoreClick(): void {
