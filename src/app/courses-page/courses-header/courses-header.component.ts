85598cb492448a62f586e9eae257cd2080a3d4c5
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2023-05-17 18:35:33 +0300

    add live search

--- src/app/courses-page/courses-header/courses-header.component.ts
+++ src/app/courses-page/courses-header/courses-header.component.ts
@@ -1,22 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ROUTES_NAMES } from '../../core/constants';
 import { CoursesService } from '../../core/courses-services/courses.service';
+import {
+    debounceTime,
+    distinctUntilChanged,
+    fromEvent,
+    map,
+    Subject,
+    switchMap,
+    throttleTime,
+} from 'rxjs';
 
 @Component({
     selector: 'app-courses-header',
     templateUrl: './courses-header.component.html',
     styleUrls: ['./courses-header.component.scss'],
 })
-export class CoursesHeaderComponent {
-    public searchValue = '';
+export class CoursesHeaderComponent implements OnInit {
     public addCourseLink = `/${ROUTES_NAMES.addCourse}`;
+    private subject$ = new Subject();
 
     constructor(private coursesService: CoursesService) {}
 
-    public onSearch(): void {
-        console.log(`Searching for ${this.searchValue}`);
-        this.coursesService.searchCourses(this.searchValue).subscribe();
+    public ngOnInit() {
+        this.handleSearch();
+    }
+
+    private handleSearch() {
+        const input = document.getElementById('searchInput')!;
+
+        fromEvent(input, 'keyup')
+            .pipe(
+                map((e) => (e?.target as HTMLInputElement).value),
+                debounceTime(500),
+                distinctUntilChanged(),
+                throttleTime(250),
+                switchMap((search) => this.coursesService.searchCourses(search))
+            )
+            .subscribe((data) => this.subject$.next(data));
+
+        this.subject$.subscribe((data) => {
+            console.log(data);
+        });
     }
 
     public onAddCourseClick(): void {
