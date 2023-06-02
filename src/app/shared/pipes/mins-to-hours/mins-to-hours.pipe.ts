7b3f51c81f4fa1b12875413d0f846279e047e3fa
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2023-06-02 13:06:23 +0300

    add translations

--- src/app/shared/pipes/mins-to-hours/mins-to-hours.pipe.ts
+++ src/app/shared/pipes/mins-to-hours/mins-to-hours.pipe.ts
@@ -1,16 +1,31 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { tap } from 'rxjs';
 
 @Pipe({
     name: 'minsToHours',
 })
 export class MinsToHoursPipe implements PipeTransform {
-    transform(value: string | number = ''): string {
+    private hours = '';
+    private mins = '';
+
+    constructor(private translate: TranslateService) {}
+
+    public transform(value: string | number = '', updated?: any): string {
         if (value) {
             const hours = Math.trunc(+value / 60);
             const mins = +value % 60;
 
+            this.hours = this.translate.instant(
+                'COURSES_PAGE.COURSE.TIME.HOURS'
+            );
+            this.mins = this.translate.instant(
+                'COURSES_PAGE.COURSE.TIME.MINUTES'
+            );
+
             return (
-                (hours >= 1 ? `${hours}h ` : '') + (mins ? `${mins}min` : '')
+                (hours >= 1 ? `${hours}${this.hours} ` : '') +
+                (mins ? `${mins}${this.mins}` : '')
             );
         }
 
